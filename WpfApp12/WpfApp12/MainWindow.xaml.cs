b638398d6feef0f0cfb75f000618b6ba11abd99c
Jazeas <mssivukhin2@gmail.com>
2026-04-09 13:40:54 +0300

    Add files via upload

---
+++ WpfApp12/WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WpfApp12
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Student> Students { get; set; }
+            = new ObservableCollection<Student>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Students.Add(new Student
+            {
+                FirstName = "Кирилл",
+                LastName = "Баранов",
+                Age = 20,
+                Group = "ВПФ-42"
+            });
+
+            Students.Add(new Student
+            {
+                FirstName = "Анна",
+                LastName = "Петрова",
+                Age = 19,
+                Group = "ВПФ-44"
+            });
+
+            DataContext = this;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Students.Add(new Student
+            {
+                FirstName = newFirstName.Text,
+                LastName = newLastName.Text,
+                Age = int.TryParse(newAge.Text, out int age) ? age : 0,
+                Group = newGroup.Text
+            });
+            newFirstName.Clear();
+            newLastName.Clear();
+            newAge.Clear();
+            newGroup.Clear();
+                
+
+        }
+        
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (studentList.SelectedItem is Student selected)
+            {
+                Students.Remove(selected);
+            }
+        }
+    }
+}
---
+++ WpfApp12/WpfApp12/Models/Student.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp12
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string group;
+
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+        public string Group
+        {
+            get => group;
+            set
+            {
+                group = value;
+                OnPropertyChanged(nameof(Group));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
